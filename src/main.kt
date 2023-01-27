a71901c97306a8ba34fc09b3d6ae6444c740e658
MatHeiUit <98590132+MatHeiUit@users.noreply.github.com>
2023-01-27 13:29:03 +0100

    Moved functions into TicTacToeGame class to fulfill requirements.

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,7 @@
 //const val TIE : Int = 3
 var GAMELOOP = true
 
-abstract class AGame {
+abstract class Game {
     // TODO: Everything. What do you mean 'everything'? EVEERYYYYTHIIIIINGGG!!!
 }
 
@@ -13,9 +13,7 @@ interface ITakeTurn {
     fun takeTurn()
 }
 
-class TicTacToeGame: AGame() {
-    //Class for rules
-    //Inherits from the superclass Game
+class TicTacToeGame: Game() {
     fun checkRows(board : GameBoard): Array<Any> {
         if (board.board[0][0] == board.board[0][1] && board.board[0][1] == board.board[0][2] && board.board[0][0] != " ") {
             return arrayOf(0, board.board[0][0])
@@ -46,27 +44,7 @@ class TicTacToeGame: AGame() {
         }
         return arrayOf(-1, false)
     }
-}
-
-
-class GameBoard {
-    //Class to represent the game board
-    // [ [0,0    0,1     0,2],
-
-    //   [1,0    1,1     1,2],
-
-    //   [2,0    2,1     2,2]]
-
-    private var _board = MutableList(3) {MutableList(3) {" "}}
-
-    var board = List(3) {List<String>(3){" "}}
-        get() = _board.toList()
-
-    fun makeMove(x: Int, y: Int, symbol: String) {
-        _board[x][y] = symbol
-    }
-
-    fun print(){
+    fun printGameBoard(board: List<List<String>>){
         val toPrint = mutableListOf<String>()
 
         for (row in board) {
@@ -90,6 +68,24 @@ class GameBoard {
         println("-".repeat(13))
         println(toPrint[2])
         println("-".repeat(13))
+    }
+    fun isValidMove(row: Int, column: Int, board: List<List<String>>): Boolean {
+        if (board[row][column] == " ") {
+            return true
+        }
+        return false
+    }
+}
+
+
+class GameBoard {
+    private var _board = MutableList(3) {MutableList(3) {" "}}
+
+    var board = List(3) {List<String>(3){" "}}
+        get() = _board.toList()
+
+    fun makeMove(x: Int, y: Int, symbol: String) {
+        _board[x][y] = symbol
     }
 
     fun printInitialBoard() {
@@ -137,21 +133,22 @@ fun varInput() : List<Int> {
             in 4..6 -> listOf(1, x - 4)
             in 7..9 -> listOf(2, x - 7)
             else -> {
-                println("Invalid move, try again:")
+                println("That's not a valid number, try again:")
                 return varInput()}
         }
     }
-    println("Invalid move, try again:")
+    println("That's not a valid number, try again")
     return varInput()
 }
 
-fun playGame(gameID: Int) { // TODO: Add option to exit mid-game.
+fun playGame(gameID: Int) {
     var moves : Int = 0
     var over : Boolean = false
     val pOne = Player("X")
     val pTwo = Player("O")
     var gameBoard = GameBoard()
     val ticTacToe = TicTacToeGame()
+    val playAgain : () -> Unit = {println("Want to play again?\n0. No\n1. Yes")}
 
     while (!over) {
         if (gameID == 1) {
@@ -161,48 +158,56 @@ fun playGame(gameID: Int) { // TODO: Add option to exit mid-game.
                 else -> currentPlayer = pTwo
             }
 
-            if (moves == 0) gameBoard.printInitialBoard()
-            else gameBoard.print()
+            if (moves == 0) {
+                gameBoard.printInitialBoard()
+            } else {
+                ticTacToe.printGameBoard(gameBoard.board)
+            }
 
             println("Player ${currentPlayer.symbol} make your move:")
             var (x, y) = varInput()
-            if (gameBoard.board[x][y] != " ") {
-                println("Invalid choice.")
-            } else {
+            if (ticTacToe.isValidMove(x, y, gameBoard.board)) {
                 gameBoard.makeMove(x, y, currentPlayer.symbol)
                 moves += 1
                 if (ticTacToe.checkColumns(gameBoard)[0] != -1) {
-                    gameBoard.print()
+                    ticTacToe.printGameBoard(gameBoard.board)
                     println("Player ${ticTacToe.checkColumns(gameBoard)[1]} wins in column ${ticTacToe.checkColumns(gameBoard)[0]}")
+                    playAgain()
                     over = true
                     continue
                 }
                 if (ticTacToe.checkRows(gameBoard)[0] != -1) {
-                    gameBoard.print()
+                    ticTacToe.printGameBoard(gameBoard.board)
                     println("Player ${ticTacToe.checkRows(gameBoard)[1]} wins in row ${ticTacToe.checkRows(gameBoard)[0]}")
+                    playAgain()
                     over = true
                     continue
                 }
                 if (ticTacToe.checkDiagonals(gameBoard)[0] != -1) {
-                    gameBoard.print()
+                    ticTacToe.printGameBoard(gameBoard.board)
                     println("Player ${ticTacToe.checkDiagonals(gameBoard)[1]} wins in diagonal ${ticTacToe.checkDiagonals(gameBoard)[0]}")
+                    playAgain()
                     over = true
                     continue
                 }
                 if (moves == 9) {
-                    gameBoard.print()
+                    ticTacToe.printGameBoard(gameBoard.board)
                     println("Game has tied.")
+                    playAgain()
                     over = true
                     continue
                 }
+            } else {
+                    println("That's not a valid move.")
+                }
             }
         }
-    }
 }
 
+
 fun main() {
+    println("Select game by entering the number...\n 1. TicTacToe\n 0. Exit")
     while (GAMELOOP) {
-        println("Select game by entering the number...\n 1. TicTacToe\n 0. Exit")
         when (readln().toInt()) {
             0 -> GAMELOOP = false
             1 -> playGame(1)
